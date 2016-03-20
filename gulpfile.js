ba78ec12be09035216dcdcbec88526e73d6d97b4
Russell Cousineau <russell.miller@rentrakmail.com>
2016-03-20 10:05:03 -0700

    add ngAnnotate/uglify to build task to create minified JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp');
 var batch = require('gulp-batch');
 var concat = require('gulp-concat');
+var ngAnnotate = require('gulp-ng-annotate');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 var watch = require('gulp-watch');
 
 gulp.task('build', function() {
     return gulp.src('src/**/*.js')
         .pipe(concat('reticulating-splines.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(ngAnnotate())
+        .pipe(uglify())
+        .pipe(rename({extname: '.min.js'}))
         .pipe(gulp.dest('dist'));
 });
 
