ef32d6a180c746edd1eac6c38c54238c41d32156
Russell Cousineau <russell.miller@rentrakmail.com>
2016-03-20 09:54:53 -0700

    add watch to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp');
+var batch = require('gulp-batch');
 var concat = require('gulp-concat');
+var watch = require('gulp-watch');
+
+gulp.task('build', function() {
+    return gulp.src('src/**/*.js')
+        .pipe(concat('reticulating-splines.js'))
+        .pipe(gulp.dest('dist'));
+});
 
 gulp.task('default', function() {
-    return gulp.src('./src/**/*.js')
-        .pipe(concat('reticulating-splines.js'))
-        .pipe(gulp.dest('./dist'));
+    watch('src/**/*.js', batch(function(events, done) {
+        gulp.start('build', done);
+    }));
 });
